55301ed0939a12a1b1bd8e9276967d3452120cf0
Jacob Turner <jacob.turner@npcc.police.uk>
2025-11-13 14:47:06 +0000

    Removes old, commented out mousekeys

--- keymap.c
+++ keymap.c
@@ -19,18 +19,6 @@
 #include "features/upside_down.h"
 #include "features/git_shorthand.h"
 
-// Mouse key speed and acceleration.
-// #undef MOUSEKEY_DELAY
-// #define MOUSEKEY_DELAY 0
-// #undef MOUSEKEY_INTERVAL
-// #define MOUSEKEY_INTERVAL 16
-// #undef MOUSEKEY_WHEEL_DELAY
-// #define MOUSEKEY_WHEEL_DELAY 0
-// #undef MOUSEKEY_MAX_SPEED
-// #define MOUSEKEY_MAX_SPEED 6
-// #undef MOUSEKEY_TIME_TO_MAX
-// #define MOUSEKEY_TIME_TO_MAX 120
-
 #undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY 10
 #undef MOUSEKEY_INTERVAL
