9c1decc840abfe47d1400f7ce088e1157e2fd2da
Jacob Turner <jacob.turner@npcc.police.uk>
2025-11-13 14:46:03 +0000

    Update to new QMK format

--- rules.mk
+++ rules.mk
@@ -3,6 +3,8 @@ UNICODE_COMMON = yes
 OS_DETECTION_ENABLE = yes
 AUTOCORRECT_ENABLE = yes
 LTO_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+MOUSE_KEYS_ENABLE = yes
 
 SRC += features/upside_down.c
 SRC += features/git_shorthand.c
